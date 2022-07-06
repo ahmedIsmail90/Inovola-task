8038afe22eecb0b67f63c035213b5420a09498c1
ahmed <ahmed@ahmeds-MacBook-Pro.local>
2022-07-06 06:06:01 +0200

    add enum for user types and order status

--- config/enum.php
+++ config/enum.php
@@ -1,1 +1,15 @@
 <?php
+return [
+    'user_types' => [
+        'ADMIN' =>  'admin',
+        'MERCHANT' => 'merchant',
+        'CUSTOMER' => 'customer',
+    ],
+    'order_status'=>[
+        'CART' => 'cart',
+        'PENDING'=>'pending',
+        'CONFIRMED'=>'confirmed',
+        'CANCELLED'=> 'cancelled'
+
+    ]
+];
