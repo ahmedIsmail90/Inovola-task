95672b630b006e955b3cffd9f95c206fb8ea9326
ahmed <ahmed@ahmeds-MacBook-Pro.local>
2022-07-06 06:18:12 +0200

    routes that need ability check middleware

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\StoreController;
+use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\CartController;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +22,20 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+    Route::middleware(['auth:sanctum','is.merchant'])->group( function () {
+    Route::resource('Store', StoreController::class);
+    Route::POST('/store/{store}/products/', [ProductController::class, 'store']);
+    Route::PUT('/products/{product}', [ProductController::class, 'update']);
+});
+
+Route::middleware(['auth:sanctum','is.customer'])->group( function () {
+
+    Route::POST('store/{store}/cart/items/', [CartController::class, 'addItems']);
+    Route::get('/cart/{cart}', [CartController::class, 'show']);
+    Route::put('/cart/{cart}/item/{item}', [CartController::class, 'removeItem']);
+
+});
+
+
